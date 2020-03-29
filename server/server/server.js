4e1785509e953323133c11c8ac56cd12cfdded16
Alastair Taft <ali.c.taft@gmail.com>
2020-03-30 08:26:24 +1100

    Headers fix for success response.

--- server/server/server.js
+++ server/server/server.js
@@ -10,16 +10,23 @@ const handleServerResponse = handler => async (...args) => {
     var result = await handler(...args)
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Methods': 'POST,OPTIONS',
+        'Access-Control-Allow-Headers':
+          'Content-Type, Content-Encoding',
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify(result, null, 2),
     }
   } catch (err) {
     console.error(err)
-    console.log('process.env.NODE_ENV', process.env.NODE_ENV)
     var statusCode = err instanceof ServerError ? err.statusCode : 500
     return {
       statusCode,
       headers: {
-        'Access-Control-Allow-Origin': args[0].headers['Origin'] || args[0].headers['origin'] || '*',
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
         'Access-Control-Allow-Methods': 'POST,OPTIONS',
         'Access-Control-Allow-Headers':
